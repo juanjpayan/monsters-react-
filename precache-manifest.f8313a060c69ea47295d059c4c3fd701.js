93385a0c7387d09b1b44b3d70656b6b168d17d5a
jpjuansito <jpjuansito@penguin>
2020-07-26 23:28:10 -0400

    Updates

---
+++ precache-manifest.f8313a060c69ea47295d059c4c3fd701.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e2ed1aeb6db8e05bd88c3f7361519a2",
+    "url": "/monsters-react-/index.html"
+  },
+  {
+    "revision": "27feafdaab3a2946903a",
+    "url": "/monsters-react-/static/css/main.fced53c8.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-react-/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-react-/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27feafdaab3a2946903a",
+    "url": "/monsters-react-/static/js/main.c35d906c.chunk.js"
+  },
+  {
+    "revision": "f814f91c906e660f2a83",
+    "url": "/monsters-react-/static/js/runtime-main.9ea075f3.js"
+  }
+]);

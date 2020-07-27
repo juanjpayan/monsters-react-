2565358f49b4947148f29cb49a6eae54966c1e7f
jpjuansito <jpjuansito@penguin>
2020-07-26 22:43:04 -0400

    added searchbox

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import './App.css';
+
 import { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/searchbox/search-box.component';
 
 class App extends Component {
 	constructor() {
 		super();
 
 		this.state = {
-			monsters: []
+			monsters: [],
+			searchField: ''
 		};
 	}
 
@@ -18,9 +20,17 @@ class App extends Component {
 			.then((users) => this.setState({ monsters: users }));
 	}
 	render() {
+		const { monsters, searchField } = this.state;
+		const filteredMonsters = monsters.filter((monster) =>
+			monster.name.toLowerCase().includes(searchField.toLowerCase())
+		);
 		return (
 			<div className="App">
-				<CardList monsters={this.state.monsters} />
+				<SearchBox
+					placeholder="search monsters"
+					handleChage={(e) => this.setState({ searchField: e.target.value })}
+				/>
+				<CardList monsters={filteredMonsters} />
 			</div>
 		);
 	}
